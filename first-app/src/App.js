156398fa22b2a221744e55a806b1e02da28880dd
mootaz2000 <mootazbendhafer@gmail.com>
2023-04-29 14:09:29 +0100

    first commit

---
+++ first-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import AddTask from './component/AddTask';
+
+import FiltreTask from './component/FiltreTask';
+import ListTask from './component/ListTask';
+import Task from './component/Task';
+import { Route, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <div >
+    <ListTask/>
+  <FiltreTask/>
+  <AddTask/>
+    </div>
+  );
+}
+
+export default App;
